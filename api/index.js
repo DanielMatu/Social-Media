251d67567e8065b1f621151ca62feac5f7428494
matudani <daniel.matu@mail.utoronto.ca>
2021-09-14 17:25:32 -0400

    tried getting rid of proxy

--- api/index.js
+++ api/index.js
@@ -55,6 +55,12 @@ app.use('/api/posts', postRoute)
 app.use('/api/conversations', conversationRoute)
 app.use('/api/messages', messageRoute)
 
+// app.use(express.static(path.join(__dirname, '/client/build', 'index.html')))
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+// })
+
 app.use(express.static(path.join(__dirname, '/client/build', 'index.html')))
 
 app.get('*', (req, res) => {
