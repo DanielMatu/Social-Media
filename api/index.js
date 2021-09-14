06a3cf91e06f8d384cdefb94a5f673a7f72361f1
matudani <daniel.matu@mail.utoronto.ca>
2021-09-14 18:21:43 -0400

    trying to fix mime error

--- api/index.js
+++ api/index.js
@@ -61,7 +61,7 @@ app.use('/api/messages', messageRoute)
 //     res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
 // })
 
-app.use(express.static(path.join(__dirname, '/client/build', 'index.html')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
