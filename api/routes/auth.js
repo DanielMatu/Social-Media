df3edc31460ae24ae6bdf25f51298657c8cd5a94
matudani <daniel.matu@mail.utoronto.ca>
2021-09-15 13:17:08 -0400

    debugging 500 server error

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -26,12 +26,12 @@ router.post('/register', async (req, res) => {
 //LOGIN
 router.post('/login', async (req, res) => {
     try{
-        const user = await User.findOne({email: req.body.email})
+        // const user = await User.findOne({email: req.body.email})
         // !user && res.status(404).send("user not found")
         // const validPassword = await bcrypt.compare(req.body.password, user.password)
         // !validPassword && res.status(400).json("wrong password")
-
-        res.status(200).json(user)
+        // res.status(200).json(user)
+        res.status(200).json('you just tried to login, didnt do anything tho :p')
     } catch (err){
         res.status(500).json(err)
     }
