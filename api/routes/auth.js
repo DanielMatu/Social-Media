8e7a100a7eef1aa013263fdfd50d1d7cec281d7f
matudani <daniel.matu@mail.utoronto.ca>
2021-09-15 14:03:32 -0400

    debugging 500 server error

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
 router.post('/register', async (req, res) => {
@@ -25,28 +26,25 @@ router.post('/register', async (req, res) => {
 //LOGIN
 router.post('/login', async (req, res) => {
     try{
-        // const user = await User.findOne({email: req.body.email})
+        const user = await User.findOne({email: req.body.email})
         // !user && res.status(404).send("user not found")
         // const validPassword = await bcrypt.compare(req.body.password, user.password)
         // !validPassword && res.status(400).json("wrong password")
         // res.status(200).json(user)
-        let user = await User.findOne({email: req.body.email})
 
-        user = {
-        "_id": "61255f908f1180332c2c23ec",
-        "profilePic": "person/1.jpeg",
-        "coverPicture": "",
-        "followers": [],
-        "isAdmin": false,
-        "username": "jane",
-        "email": "jane@gmail.com",
-        "password": "$2b$10$6gP7EkLDBH2y4Dxl.phKHOVI6BVpiXH2DQZ14/AuTzY7IIkPNFHpC",
+    //     user = {
+    //     "_id": "61255f908f1180332c2c23ec",
+    //     "profilePic": "person/1.jpeg",
+    //     "coverPicture": "",
+    //     "followers": [],
+    //     "isAdmin": false,
+    //     "username": "jane",
+    //     "email": "jane@gmail.com",
+    //     "password": "$2b$10$6gP7EkLDBH2y4Dxl.phKHOVI6BVpiXH2DQZ14/AuTzY7IIkPNFHpC",
 
-        "followings": [1,2,3]
-    }
-
-
-        res.status(200).json(user)
+    //     "followings": [1,2,3]
+    // }
+        res.status(200).json('user')
     } catch (err){
         res.status(500).json(err)
     }
