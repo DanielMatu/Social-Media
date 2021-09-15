a126f7d83318a794d185a1108b568d55ca924aa0
matudani <daniel.matu@mail.utoronto.ca>
2021-09-15 15:53:16 -0400

    debugging 500 server error

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,17 +4,22 @@ const bcrypt = require('bcrypt')
 
 router.post('/register', async (req, res) => {
     try{
-        // generate new password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        // // generate new password
+        // const salt = await bcrypt.genSalt(10)
+        // const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
-        // create new user
+        // // create new user
+        // const newUser = new User({
+        //     username:req.body.username,
+        //     email:req.body.email,
+        //     password: hashedPassword
+        // })
+
         const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password: hashedPassword
+            username: 'testuser',
+            email: 'test@test.com',
+            password: 'testpassword'
         })
-
         // save user and respond
         const user = await newUser.save()
         res.status(200).json(user)
@@ -26,26 +31,12 @@ router.post('/register', async (req, res) => {
 //LOGIN
 router.post('/login', async (req, res) => {
     try{
-        // const user = await User.findOne({email: req.body.email})
-        // !user && res.status(404).send("user not found")
-        // const validPassword = await bcrypt.compare(req.body.password, user.password)
-        // !validPassword && res.status(400).json("wrong password")
-        // res.status(200).json(user)
-
-    //     user = {
-    //     "_id": "61255f908f1180332c2c23ec",
-    //     "profilePic": "person/1.jpeg",
-    //     "coverPicture": "",
-    //     "followers": [],
-    //     "isAdmin": false,
-    //     "username": "jane",
-    //     "email": "jane@gmail.com",
-    //     "password": "$2b$10$6gP7EkLDBH2y4Dxl.phKHOVI6BVpiXH2DQZ14/AuTzY7IIkPNFHpC",
-
-    //     "followings": [1,2,3]
-    // }
-        console.log(User.prototype.db.name)
-        res.status(200).json(User.prototype.db.name)
+        const user = await User.findOne({email: req.body.email})
+        !user && res.status(404).send("user not found")
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password")
+        res.status(200).json(user)
+        // console.log(User.prototype.db.name)
     } catch (err){
         res.status(500).json(err)
     }
