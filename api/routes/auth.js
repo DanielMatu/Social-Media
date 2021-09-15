3309b9a04b062ed044e654b6d85adb437d7ba99c
matudani <daniel.matu@mail.utoronto.ca>
2021-09-15 16:06:06 -0400

    debugging 500 server error

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -21,7 +21,7 @@ router.post('/register', async (req, res) => {
             password: 'testpassword'
         })
         // save user and respond
-        const user = await newUser.save()
+        // const user = await newUser.save()
         res.status(200).json('user')
     } catch (err){
         res.status(500).json(err)
