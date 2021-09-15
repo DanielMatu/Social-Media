fae2048966a830bba1c08745fd82962fcdb151fd
matudani <daniel.matu@mail.utoronto.ca>
2021-09-15 11:55:05 -0400

    added cors middleware to backend server

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -4,6 +4,7 @@ let axiosInstance
 
 if (process.env.NODE_ENV === 'development') {
     axiosInstance = axios.create({
+        baseURL: 'http://localhost:8800/api/'
     })
 } else {
     axiosInstance = axios.create({
